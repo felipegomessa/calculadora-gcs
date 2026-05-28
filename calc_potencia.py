858aa5a94e5da2ee3bc9897a68f6828320c5d545
Spare-pixel <sparemirror@gmail.com>
2026-05-28 04:10:38 -0400

    calc_potencia
    feat: implementa operações de potenciação (potencia, raiz_quadrada, raiz_cubica)

---
+++ calc_potencia.py
@@ -0,0 +1,14 @@
+import math;
+
+int potencia(int a, int b) {
+    return math.pow(a,b)
+}
+
+int raiz_quadrada(int a) {
+    return math.sqrt(a)
+}
+
+int raiz_cubica(int a) {
+    return math.cbrt(a)
+}
+
