36ee59e32976e0a5ad3ec931c373170a841d409b
Spare-pixel <sparemirror@gmail.com>
2026-05-28 04:24:48 -0400

    Add files via upload
    feat: implementa operações de conversão (celsius para fahrenheit, km para milhas, kg para libras)

---
+++ calc_conversao.py
@@ -0,0 +1,11 @@
+float celsius_para_farenheit(float a) {
+    return (a * 1.8) + 32
+}
+
+float km_para_milhas(float a) {
+    return a*0,62137119 
+}
+
+float kg_para_libras(float a) {
+    return a*2,20462262
+}
